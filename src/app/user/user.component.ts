b8cece567da5ab8a64ad0de0b36286795136f023
SalmaHossam <11410120206031@stud.edu.eg>
2024-12-12 16:36:22 +0200

    user commponent enhancments

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+// const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+  //using signals
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath=computed(()=>'assets/users/'+this.selectedUser().avatar)
+  
+  //using zone.js
+  // get impagePath(){
+  //   return 'assets/users/'+ this.selectedUser.avatar
+  // }
+
+  onSelectUser(){
+    // const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
+    // this.selectedUser.set(DUMMY_USERS[randomIndex])
+  }
 
 }
